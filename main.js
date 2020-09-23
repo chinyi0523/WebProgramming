7dee26f5f572aa373dfa2383603976ede8940d4f
chinyi0523 <james31423a@gmail.com>
2020-09-23 11:55:15 +0800

    add onload

--- main.js
+++ main.js
@@ -4,18 +4,15 @@ var cnt = 0
 let loading = new Image();
 loading.src = './images/loading.gif'
 
-// function load(cnt){
-//     let target = document.getElementById("display");
-//     target.src = loading.src
-//     var timer = setInterval(function(){
-//                 if (document.getElementById("display").complete){
-//                 clearInterval(timer);
-//                 target.src = img_urls[cnt]
-//                 }
-//                 }, 10);
-//     document.getElementById("source").innerHTML = "Source: "+img_urls[cnt]
-// }
-function loaded(cnt){
+window.onload = function (){
+    let target = document.getElementById("display");
+    target.src = loading.src
+    target.src = img_urls[0]
+    document.getElementById("source").innerHTML = img_urls[0]
+    document.getElementById("source").href = img_urls[0]
+}
+
+function load(cnt){
     let target = document.getElementById("display");
     target.src = loading.src
     target.src = img_urls[cnt]
@@ -27,7 +24,7 @@ function select(nxt){
     if(nxt===true){
         if(cnt!==(img_urls.length-1)){
             cnt+=1
-            loaded(cnt)
+            load(cnt)
             
             document.getElementById("prevImg").classList.remove("disabled");
             if(cnt===(img_urls.length-1)){
@@ -41,7 +38,7 @@ function select(nxt){
     else{
         if(cnt!==0){
             cnt-=1
-            loaded(cnt)
+            load(cnt)
 
             document.getElementById("nextImg").classList.remove("disabled");
             if(cnt===0){
